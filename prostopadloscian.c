fe5d0b07b160cb7d719de298cecf72a915732d0f
Zuzanna Jerz <22597@student.konin.edu.pl>
2026-01-16 10:55:42 +0100

    dodano menu.

--- prostopadloscian.c
+++ prostopadloscian.c
@@ -1,4 +1,5 @@
-// zadanie 17 - poczatkowy kod
+
+// zadanie 17 - menu
 
 #include <stdio.h>
 #include <math.h>
@@ -6,7 +7,12 @@
 float volume_sphere(float r) {
 	return (4.0/3.0) * M_PI * r * r * r;
 }
-	void calculate_volume_sphere() {
+
+float volume_cuboid(float a, float b, float h) {
+	return a * b * h;
+}
+
+void calculate_volume_sphere() {
 	float r;
 
 	printf("Podaj promien kuli: ");
@@ -14,31 +20,41 @@ float volume_sphere(float r) {
 
 	printf("Objetosc kuli: %f\n", volume_sphere(r));
 }
-//float volume_cuboid(float a, float b, float h) {
-//	return(a*b*h);
-//}
 
-//void calculate_volume_cuboid() {
-//	float a, b, h;
+void calculate_volume_cuboid() {
+	float a, b, h;
 
-//	printf("Podaj dlugosc boku a: ");
-//	scanf("%f", &a);
+	printf("Podaj dlugosc boku a: ");
+	scanf("%f", &a);
 
-//	printf("Podaj dlugosc boku b: ");
-//	scanf("%f", &b);
+	printf("Podaj dlugosc boku b: ");
+	scanf("%f", &b);
 
-//	printf("Podaj wysokosc h: ");
-//	scanf("%f", &h);
+	printf("Podaj wysokosc h: ");
+	scanf("%f", &h);
 
-//	printf("Objetosc prostopadloscianu: %f\n", volume_cuboid(a, b, h));
-//}
-
-
-int main() {
-    //calculate_volume_cuboid();
-	calculate_volume_sphere();
-
-	return(0);
+	printf("Objetosc prostopadloscianu: %f\n", volume_cuboid(a, b, h));
 }
 
+int main() {
+	int wybor;
+
+	printf("MENU\n");
+	printf("1 - Oblicz objetosc kuli\n");
+	printf("2 - Oblicz objetosc prostopadloscianu\n");
+	printf("Wybierz opcje: ");
+	scanf("%d", &wybor);
+
+	if (wybor == 1) {
+		calculate_volume_sphere();
+	}
+	else if (wybor == 2) {
+		calculate_volume_cuboid();
+	}
+	else {
+		printf("Nieprawidlowy wybor!\n");
+	}
+
+	return 0;
+}
 
