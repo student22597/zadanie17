6903ec00686d7aadd2684471e8fbfe50abb3c273
Zuzanna Jerz <22597@student.konin.edu.pl>
2026-01-16 10:50:06 +0100

    dodano kalkulator objętości kuli.

--- prostopadloscian.c
+++ prostopadloscian.c
@@ -1,29 +1,44 @@
 // zadanie 17 - poczatkowy kod
 
 #include <stdio.h>
+#include <math.h>
 
-float volume_cuboid(float a, float b, float h) {
-	return(a*b*h);
+float volume_sphere(float r) {
+	return (4.0/3.0) * M_PI * r * r * r;
 }
+	void calculate_volume_sphere() {
+	float r;
 
-void calculate_volume_cuboid() {
-	float a, b, h;
+	printf("Podaj promien kuli: ");
+	scanf("%f", &r);
 
-	printf("Podaj dlugosc boku a: ");
-	scanf("%f", &a);
-
-	printf("Podaj dlugosc boku b: ");
-	scanf("%f", &b);
-
-	printf("Podaj wysokosc h: ");
-	scanf("%f", &h);
-
-	printf("Objetosc prostopadloscianu: %f\n", volume_cuboid(a, b, h));
+	printf("Objetosc kuli: %f\n", volume_sphere(r));
 }
+//float volume_cuboid(float a, float b, float h) {
+//	return(a*b*h);
+//}
+
+//void calculate_volume_cuboid() {
+//	float a, b, h;
+
+//	printf("Podaj dlugosc boku a: ");
+//	scanf("%f", &a);
+
+//	printf("Podaj dlugosc boku b: ");
+//	scanf("%f", &b);
+
+//	printf("Podaj wysokosc h: ");
+//	scanf("%f", &h);
+
+//	printf("Objetosc prostopadloscianu: %f\n", volume_cuboid(a, b, h));
+//}
 
 
 int main() {
-	calculate_volume_cuboid();
+    //calculate_volume_cuboid();
+	calculate_volume_sphere();
 
 	return(0);
 }
+
+
